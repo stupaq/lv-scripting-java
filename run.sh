b461563a2c58b964cf6a03865208750cff986d1d
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-06-18 13:17:27 +0200

    ActiveX create formula works. Cleaned DLL stuff.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/c/Program\ Files/Java/jdk1.7.0_60/bin/java.exe \
-    -cp "target/lib/*;target/classes/" \
+/c/Program\ Files/Java/jdk1.7.0_60/bin/java.exe -cp "target/lib/*;target/classes/" \
+    -Dcom.jacob.debug=false -Djava.library.path="target/lib/" \
     stupaq.Main \
 
 read
--- src/main/java/stupaq/Main.java
+++ src/main/java/stupaq/Main.java
@@ -2,10 +2,6 @@ package stupaq;
 
 import com.jacob.activeX.ActiveXComponent;
 import com.jacob.com.Variant;
-import com.sun.jna.Memory;
-import com.sun.jna.Native;
-
-import stupaq.scripting.ScriptingLibrary;
 
 @SuppressWarnings("deprecation")
 public class Main {
@@ -30,22 +26,5 @@ public class Main {
     Variant res = vi.invoke("Run");
     System.out.println(res.toString());
     // TODO
-  }
-
-  private static void sharedLibrary() throws Exception {
-    ScriptingLibrary instance =
-        (ScriptingLibrary) Native.loadLibrary("scripting", ScriptingLibrary.class);
-    Memory path = stringPointer("target3.vi");
-    int len = 2048;
-    Memory err = new Memory(len + 1);
-    err.setMemory(0, len, (byte) 0);
-    instance.CreateFormula(path, err, len);
-    System.out.println(err.getString(0));
-  }
-
-  private static Memory stringPointer(String str) {
-    Memory ptr = new Memory(str.length() + 1);
-    ptr.setString(0, str);
-    return ptr;
   }
 }
