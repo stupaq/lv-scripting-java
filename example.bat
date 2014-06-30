967bf4987248246e1330c19414388894457db49e
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-06-30 14:06:43 +0200

    Moved VIs as submodule of the compiler package.

--- example.bat
+++ example.bat
@@ -1,6 +1,5 @@
-@SET scripting_dir=..\..\lv-scripting
 @SET target_vi=..\..\testing\example.vi
 
-"C:\Program Files\Java\jdk1.7.0_60\bin\java.exe" -cp "target\classes;target\dependency\*" -Dscripting.tools.path="%scripting_dir%" example "%target_vi%"
+"C:\Program Files\Java\jdk1.7.0_60\bin\java.exe" -cp "target\classes;target\dependency\*" -Dscripting.tools.path="..\lv-scripting" example "%target_vi%"
 
 @PAUSE
