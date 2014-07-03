25c3855906916f8512d8a982771dcd74780800e8
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-07-03 02:08:50 +0200

    Wire owner setting fixup.

--- src/main/java/example.java
+++ src/main/java/example.java
@@ -40,21 +40,21 @@ public class example {
       Terminal vi0t2 = vi0b1.addOutput("this output will be removed");
       Formula vi0b2 = new FormulaNode(vi0, "block 2", of("label 2"));
       Terminal vi0t3 = vi0b2.addInput("in 1");
-      Wire vi0w1 = new Wire(vi0t1, vi0t3, of("wire 1"));
+      Wire vi0w1 = new Wire(vi0, vi0t1, vi0t3, of("wire 1"));
       Formula vi0b3 = new FormulaNode(vi0, "this node will be removed", of("this label too"));
       Control vi0c0 = new Control(vi0, ControlCreate.NUMERIC, "control 0", 0);
       Indicator vi0i0 =
           new Indicator(vi0, ControlCreate.NUMERIC, "indicator 0", ControlCreate.DO_NOT_CONNECT);
       Indicator vi0i1 =
           new Indicator(vi0, ControlCreate.NUMERIC, "indicator 1", ControlCreate.DO_NOT_CONNECT);
-      Wire vi0w2 = new Wire(vi0c0.terminal(), vi0i0.terminal(), of("wire 2"));
+      Wire vi0w2 = new Wire(vi0, vi0c0.terminal(), vi0i0.terminal(), of("wire 2"));
       VIPath path1 = new VIPath(Paths.get(args[0]), "example1.vi");
       Files.deleteIfExists(path1.path());
       VI vi1 = new VI(tools, path1);
       vi1.create();
       Indicator v1i1 = new Indicator(vi1, ControlCreate.NUMERIC, "indicator 0", 0);
       SubVI vi0sub0 = new SubVI(vi0, path1, "sub vi 0");
-      Wire vi0w3 = new Wire(vi0sub0.terminals().get(0), vi0i1.terminal(), of("wire 3"));
+      Wire vi0w3 = new Wire(vi0, vi0sub0.terminals().get(0), vi0i1.terminal(), of("wire 3"));
       vi0.cleanUpDiagram();
       vi0b3.delete();
       //t2.delete();
--- src/main/java/stupaq/labview/scripting/hierarchy/Wire.java
+++ src/main/java/stupaq/labview/scripting/hierarchy/Wire.java
@@ -6,17 +6,13 @@ import stupaq.labview.UID;
 import stupaq.labview.scripting.tools.WireConnect;
 
 public final class Wire extends ConcreteGObject {
-  public Wire(GObject owner, UID uid) {
+  public Wire(Generic owner, UID uid) {
     super(owner, uid);
   }
 
-  public Wire(Terminal source, Terminal sink, Optional<String> label) {
-    this(source, source.scriptingTools()
+  public Wire(Generic owner, Terminal source, Terminal sink, Optional<String> label) {
+    this(owner, source.scriptingTools()
         .getTool(WireConnect.class)
         .apply(source.viPath(), source.uid().get(), sink.uid().get(), label));
-  }
-
-  public Wire(Terminal source, Terminal sink) {
-    this(source, sink, Optional.<String>absent());
   }
 }
