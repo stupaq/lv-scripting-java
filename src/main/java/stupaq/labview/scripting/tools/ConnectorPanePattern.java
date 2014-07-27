5af5909fb03c0e1f66924035dee61a552a530222
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-07-28 00:45:23 +0200

    More layouts.

--- src/main/java/stupaq/labview/scripting/tools/ConnectorPanePattern.java
+++ src/main/java/stupaq/labview/scripting/tools/ConnectorPanePattern.java
@@ -15,6 +15,16 @@ public enum ConnectorPanePattern implements ActiveXType {
   P4802(4802),
   P4803(4803),
   P4805(4805),
+  P4808(4808),
+  P4811(4811),
+  P4814(4814),
+  P4815(4815),
+  P4823(4823),
+  P4825(4825),
+  P4831(4831),
+  P4832(4832),
+  P4833(4833),
+  P4834(4834),
   P4835(4835);
   private static final TreeMap<Integer, ConnectorPanePattern> countToPattern = Maps.newTreeMap();
 
@@ -23,10 +33,20 @@ public enum ConnectorPanePattern implements ActiveXType {
     countToPattern.put(2, P4801);
     countToPattern.put(3, P4803);
     countToPattern.put(4, P4805);
+    countToPattern.put(5, P4808);
+    countToPattern.put(6, P4811);
+    countToPattern.put(7, P4814);
+    countToPattern.put(8, P4815);
+    countToPattern.put(9, P4823);
+    countToPattern.put(10, P4825);
+    countToPattern.put(11, P4831);
+    countToPattern.put(12, P4832);
+    countToPattern.put(16, P4833);
+    countToPattern.put(20, P4834);
     countToPattern.put(28, P4835);
   }
 
-  public static int DO_NOT_CONNECT = -1;
+  public static final int DO_NOT_CONNECT = -1;
   private final int pattern;
 
   private ConnectorPanePattern(int pattern) {
