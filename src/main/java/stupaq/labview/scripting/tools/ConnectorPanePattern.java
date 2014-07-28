a34680c6e060b62be41905872902567d7f81b7de
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-07-28 23:02:30 +0200

    Fixed patterns lookup table.

--- src/main/java/stupaq/labview/scripting/tools/ConnectorPanePattern.java
+++ src/main/java/stupaq/labview/scripting/tools/ConnectorPanePattern.java
@@ -1,50 +1,47 @@
 package stupaq.labview.scripting.tools;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Maps;
+import com.google.common.collect.ImmutableSortedMap;
 
 import com.jacob.com.Variant;
 import com.jacob.extensions.ActiveXType;
 
 import java.util.Map.Entry;
-import java.util.TreeMap;
 
 public enum ConnectorPanePattern implements ActiveXType {
   P4800(4800),
   P4801(4801),
   P4802(4802),
-  P4803(4803),
   P4805(4805),
-  P4808(4808),
+  P4807(4807),
+  P4810(4810),
   P4811(4811),
-  P4814(4814),
+  P4812(4812),
+  P4813(4813),
   P4815(4815),
-  P4823(4823),
-  P4825(4825),
-  P4831(4831),
-  P4832(4832),
+  P4826(4826),
+  P4829(4829),
   P4833(4833),
   P4834(4834),
   P4835(4835);
-  private static final TreeMap<Integer, ConnectorPanePattern> countToPattern = Maps.newTreeMap();
-
-  static {
-    countToPattern.put(1, P4800);
-    countToPattern.put(2, P4801);
-    countToPattern.put(3, P4803);
-    countToPattern.put(4, P4805);
-    countToPattern.put(5, P4808);
-    countToPattern.put(6, P4811);
-    countToPattern.put(7, P4814);
-    countToPattern.put(8, P4815);
-    countToPattern.put(9, P4823);
-    countToPattern.put(10, P4825);
-    countToPattern.put(11, P4831);
-    countToPattern.put(12, P4832);
-    countToPattern.put(16, P4833);
-    countToPattern.put(20, P4834);
-    countToPattern.put(28, P4835);
-  }
+  private static final ImmutableSortedMap<Integer, ConnectorPanePattern> countToPattern =
+      ImmutableSortedMap.<Integer, ConnectorPanePattern>naturalOrder()
+          .put(1, P4800)
+          .put(2, P4801)
+          .put(3, P4802)
+          .put(4, P4805)
+          .put(5, P4807)
+          .put(6, P4810)
+          .put(7, P4811)
+          .put(8, P4812)
+          .put(9, P4813)
+          .put(10, P4826)
+          .put(11, P4829)
+          .put(12, P4815)
+          .put(16, P4833)
+          .put(20, P4834)
+          .put(28, P4835)
+          .build();
 
   public static final int DO_NOT_CONNECT = -1;
   private final int pattern;
